4c600f9d656886d52588986c95d56ed30f0da29f
Rach Radulovich <rachradulo@users.noreply.github.com>
2025-10-16 16:34:36 -0400

    fix(designer): fix build error

--- designer/src/components/Rag/AddRetrievalStrategy.tsx
+++ designer/src/components/Rag/AddRetrievalStrategy.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useActiveProject } from '../../hooks/useActiveProject'
