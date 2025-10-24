57fa504020b0e4648aaf3d269b8d347224282337
rgthelen <rgthelen@gmail.com>
2025-10-24 14:56:29 -0500

    fix(designer): remove @codemirror/lint from vite config - package not installed

--- designer/vite.config.ts
+++ designer/vite.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
             '@codemirror/commands',
             '@codemirror/lang-json',
             '@codemirror/lang-yaml',
-            '@codemirror/lint',
             '@codemirror/search',
             '@codemirror/theme-one-dark',
             '@lezer/highlight',
